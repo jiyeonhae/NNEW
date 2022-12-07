7cb26aec1284833b64c3d9049e4e1baac87b8d49
jiyeonhae <bjy51188@gmail.com>
2022-12-07 21:17:33 +0900

    1207ss

---
+++ spring1207/src/main/java/com/example/spring1207/Spring1207Application.java
@@ -0,0 +1,13 @@
+package com.example.spring1207;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1207Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1207Application.class, args);
+	}
+
+}
---
+++ spring1207/src/test/java/com/example/spring1207/Spring1207ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring1207;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1207ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
